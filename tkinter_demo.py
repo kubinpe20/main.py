9c69e2fc237f82607d02880050d9974fad9f2728
kubinpe20 <kubinpe20@sps-prosek.cz>
2022-02-24 15:41:48 +0100

    okno zaklad

---
+++ tkinter_demo.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry("250x250")
+window.title("aplikace v1.1 alfa a omega")
+label = tk.Label(text=("bordel"))
+button = tk.Button
+label.pack()
+
+window.mainloop()
